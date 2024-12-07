084441851b1d7b69d1fb425b0ca07c489f538798
Aditya Khowal <108611217+AdityaKhowalGithub@users.noreply.github.com>
2024-12-06 23:24:25 -0800

    Quartz sync: Dec 6, 2024, 11:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "adityakhowal.com",
     ignorePatterns: ["private", "templates", ".obsidian","!(PublicMedia)**/!(*.md)", "!(*.md)"],
     defaultDateType: "created",
     theme: {
